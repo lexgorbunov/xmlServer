a34717e8eeca84de21e3be6f5f0e498d19a66aa1
LeXGorbunov <lexgorbunov@gmail.com>
2011-06-30 16:43:25 +0400

    Содержимое XML герерится автоматом на основании того что лежит в каталогах

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+$pathStatic='http://lglab.ru/xmlserver/static';
 
 $dom = new DOMDocument('1.0', 'utf-8');
 
@@ -13,19 +14,49 @@ $gameServer->appendChild($game);
 
 $images=$dom->createElement('images');
 $game->appendChild($images);
-
-$basePath='http://lglab.ru/xmlserver/static/img/';
-$imgList=array(
-    'star'=>$basePath.'s.png',
-    'wallpaper'=>$basePath.'w.jpeg',
-);
-
+$pathImg=$pathStatic.'/img';
+$imgList=array();
+$dir= dirname(__FILE__).'/static/img';
+if ($dh = opendir($dir))
+{
+    while (($file = readdir($dh)) !== false)
+    {
+        if(!is_file($dir.'/'.$file))
+            continue;
+        $imgList[preg_replace('/(.*)\.[\w\d]+$/', '$1', $file)]=$pathImg.'/'.$file;
+    }
+    closedir($dh);
+}
 foreach ($imgList as $imgID=>$imgPath)
 {
     $imgNode=$dom->createElement('img');
     $imgNode->setAttribute('id', $imgID);
     $imgNode->setAttribute('url', $imgPath);
     $images->appendChild($imgNode);
+}
+
+$sound=$dom->createElement('sound');
+$game->appendChild($sound);
+$pathSound=$pathStatic.'/sound';
+$dir= dirname(__FILE__).'/static/sound';
+$soundList=array();
+if ($dh = opendir($dir))
+{
+    while (($file = readdir($dh)) !== false)
+    {
+        if(!is_file($dir.'/'.$file))
+            continue;
+        $soundList[preg_replace('/(.*)\.[\w\d]+$/', '$1', $file)]=$pathSound.'/'.$file;
+    }
+    closedir($dh);
+}
+
+foreach ($soundList as $soundID=>$soundPath)
+{
+    $soundNode=$dom->createElement('track');
+    $soundNode->setAttribute('id', $soundID);
+    $soundNode->setAttribute('url', $soundPath);
+    $sound->appendChild($soundNode);
 }
 
 
